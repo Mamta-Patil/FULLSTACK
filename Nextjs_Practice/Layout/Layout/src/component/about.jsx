7380a77f84ee624685acb039d38d442c97f2a087
MamtaPatil <mamtapatil5252@gmail.com>
2025-03-24 18:55:38 +0530

    update

---
+++ Nextjs_Practice/Layout/Layout/src/component/about.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+      <div>
+
+      </div>
+      <div className='flex'>
+            <div>
+                <div>
+
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+            <div></div>
+            <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default about
---
+++ Nextjs_Practice/Layout/Layout/src/data/propertyplans.jsx
@@ -0,0 +1,21 @@
+export const propertyplans=[
+    {
+        name:"square feet",
+        amount:"2,537"
+    },
+    {
+        name:"car parking",
+        amount:"2"
+    },{
+        name:"half baths",
+        amount:"4"
+    },
+    {
+        name:"full baths",
+        amount:"2"
+    },
+    {
+        name:"bed room",
+        amount:"5"
+    }
+]
