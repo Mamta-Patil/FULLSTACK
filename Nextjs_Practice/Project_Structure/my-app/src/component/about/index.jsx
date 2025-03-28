00a1bf776271265874a34cc0571ea9d63cdf85bd
MamtaPatil <mamtapatil5252@gmail.com>
2025-03-28 16:17:16 +0530

    add_about_section

--- Nextjs_Practice/Project_Structure/my-app/src/component/about/index.jsx
+++ Nextjs_Practice/Project_Structure/my-app/src/component/about/index.jsx
@@ -8,6 +8,9 @@ const About = () => {
       <h2>This is About Page</h2>
       <About1 />
       <About2 />
+
+    <p className='text-[pink]'> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium architecto eligendi iusto, distinctio nesciunt, modi tempore tempora doloremque sint cum, veritatis culpa quaerat deleniti ad vel! Minus sapiente quia placeat. </p>
+
     </div>
   )
 }
