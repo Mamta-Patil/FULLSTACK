718413d6091b86232388d58efe7363d078b6f43a
MamtaPatil <mamtapatil5252@gmail.com>
2025-04-22 14:42:35 +0530

    update

--- CRUD_FULLSTACK _Copy/Server/config/server.js
+++ CRUD_FULLSTACK _Copy/Server/config/server.js
@@ -1,4 +1,20 @@
-// const mongoose=require("mogoose");
-const mongoose=require("mongoose")
-const connection=mongoose.connect("mongodb://127.0.0.1:27017/STUDENT_INFORMATION")
-module.exports=connection
+// // const mongoose=require("mogoose");
+// // const mongoose=require("mongoose")
+// // const connection=mongoose.connect(process.env.MONGO_URI)
+// // module.exports=connection
+
+
+
+// const mongoose = require("mongoose");
+
+// const connection = mongoose.connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+// .then(() => console.log("MongoDB connected successfully"))
+// .catch(err => {
+//     console.error("MongoDB connection error:", err);
+//     throw err; // Ensure the error propagates to index.js
+// });
+
+// module.exports = connection;
--- CRUD_FULLSTACK _Copy/Server/index.js
+++ CRUD_FULLSTACK _Copy/Server/index.js
@@ -1,25 +1,68 @@
+// const dotenv=require("dotenv")
 const express=require("express")
-const dotenv=require("dotenv")
-const connection = require("./config/server")
+const mongoose = require("mongoose")
+
+// const connection = require("./config/server")
 const studentRouter = require("./routes/studentRoutes")
 const app=express()
 const cors=require("cors")
-dotenv.config()
-app.use(cors({
-    origin:["http://localhost:5173","http://localhost:3000" , ],
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-    allowedHeaders: 'Content-Type',
-    credentials:true
-}));
-
+require("dotenv").config();
+app.use(cors());
 app.use(express.json())
 
+// app.use(cors({
+//     origin:["http://localhost:5173","http://localhost:3000" , ],
+//     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+//     allowedHeaders: 'Content-Type',
+//     credentials:true
+// }));
+
+// app.use(express.json())
+
 app.use("/studentsinfo",studentRouter)
-app.listen(process.env.PORT || 3000 ,async()=>{
-    try {
-        await connection
-        console.log(`server is running port on ${process.env.PORT || 3000}`)
-    } catch (error) {
-        console.log(error)
+// PORT=8080;
+// app.listen(PORT || 3000 ,async()=>{
+//     try {
+//         await connection
+//         console.log(`server is running port on ${process.env.PORT || 3000}`)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// })  
+
+
+
+
+// const express = require("express")
+// const mongoose = require("mongoose")
+// const BookRouter = require("./routes/routes")
+// // const app = express()
+// const cors = require("cors");
+// require("dotenv").config();
+
+// app.use(cors());
+// app.use(express.json())
+// app.use("/books", BookRouter)
+
+const connectToDb = async () => 
+{
+    try 
+    {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("connected to db");
     }
-})  
+    catch (error) 
+    {
+        console.log(error);
+        // mamtapatil5252
+        // f2wPK6qUygDJRln2
+    }
+}
+
+connectToDb();
+
+const PORT = 8080
+app.listen(PORT, () => 
+{
+    console.log("Server running on PORT");
+});
